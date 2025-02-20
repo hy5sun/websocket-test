e93d8b24a9af5a417cdec8ebca329be3284928aa
hy5sun <ssunykkim@naver.com>
2025-02-20 17:23:35 +0900

    feat: JPA Auditing 활성화

--- src/main/java/tossiltossil/backend/WebsocketTestApplication.java
+++ src/main/java/tossiltossil/backend/WebsocketTestApplication.java
@@ -2,8 +2,10 @@ package tossiltossil.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WebsocketTestApplication {
 
 	public static void main(String[] args) {
